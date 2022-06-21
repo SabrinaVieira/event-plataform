f980513f9316b3a748075c08f3daddb319e175a3
SabrinaVieira <sah.luciana@gmail.com>
2022-06-20 23:09:59 -0300

    :tada: tailwind intalado e  configurado

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // quais são os meus arquivos que vao utilizar o tailwind
+    './src/**/*.tsx'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
